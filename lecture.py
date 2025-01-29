1c2b27972bb2ac28ad312bc2cb26ac564e99ad5e
BienvenidoVelez <bvelez@axiomatica.net>
2025-01-29 09:49:03 -0400

    Added example templates

--- lecture.py
+++ lecture.py
@@ -1,7 +1,43 @@
-print(type(5))
-print(int('56'))
+# CIIC 3015 Intro to Programming
+# Topic: Expressions and Variables
+# Lecture 1 Live Coding Examples
 
-name = input('Enter your name: ')
-age = int(input('Enter your age in years: '))
-print(type(age))
-print("Hello", name)
+# Some common calculations used in engineering and live
+
+# Converting Fahrenheit to Celsius
+# Use input() to prompt the user for the temperature in Fahrenheit:
+# YOUR CODE HERE
+
+# Converting Celsius to Fahrenheit
+# Use input() to prompt the user for the temperature in Celsius:
+# YOUR CODE HERE
+
+# Perimeter of a rectangle
+# Use input() to prompt the user for height and width
+# YOUR CODE HERE
+
+# Area of a Circle
+# Use input() to prompt the user for radius
+# YOUR CODE HERE
+
+# Distance between two points (x1,y1) and (x2,y2)
+# Create variables to store coordinates x1, y1, x2, y2
+# YOUR CODE HERE
+
+# Savings after one year earning simple interest
+# Create variables to store annual_interest and initial_balance
+# YOUR CODE HERE
+
+# Savings after n years earning simple interest
+# Create variables to store annual_interest, initial_balance and number of years
+# YOUR CODE HERE
+
+# Quadratic Formula to find zeros of polynomial ax^2 + bx + c
+# Create variables to store coefficients a, b, c
+# YOUR CODE HERE
+
+# Area of a triangle given the lengths of its 3 sides a, b, and c (Heron's Formula)
+# Create variables to store lengths of three sides side1, side2, side3
+# YOUR CODE HERE
+
+print('End of Examples')
