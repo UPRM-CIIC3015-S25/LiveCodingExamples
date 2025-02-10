6528773afaa7374cc5ed40bbca4fdd4f67dcc2a8
BienvenidoVelez <bvelez@axiomatica.net>
2025-02-10 14:37:37 -0400

    Added questions for lecture2 without answers

--- Lecture2/Lecture2.py
+++ Lecture2/Lecture2.py
@@ -1,2 +1,35 @@
-print("Hello Lecture 2!"
-      "")
+# Use an if statement to calculate your final CIIC 3015
+# letter grade given your final numeric score
+numeric_score = 79.9999999
+
+# YOUR CODE HERE
+letter_grade = 'A'
+print(f"A score of {numeric_score} yields a {letter_grade}")
+
+# Use an if statement to calculate the number of real
+# roots of a polynomial with float coefficients a, b, c
+a = 1.0
+b = 5.0
+c = 6.0
+# Your code here
+# print(f"Polynomial {a}x^2 + {b}x + {c} has {real_roots} real roots")
+
+
+# Use if statements to determine the color resulting from
+# combining two of primary colors red, blue and yellow.
+color1 = 'blue'
+color2 = 'red'
+
+# YOUR CODE HERE
+
+# print(f"Combine {color1} with {color2} to get {combined_color}")
+
+# Use if statement to implement a "Change for a Dollar"
+# game.  The user enters an amount of quarters, dimes,
+# nickles and pennies and wins if these coins add up to
+# one dollar
+# quarters = int(input("Number of quarters: "))
+# dimes = int(input("Number of dimes: "))
+# nickles = int(input("Number of nickles: "))
+# pennies = int(input("Number of pennies: "))
+# YOUR CODE HERE
