737e5573b3b1ded0a299be84d095559ebd2bcf95
BienvenidoVelez <bvelez@axiomatica.net>
2025-02-10 14:06:22 -0400

    Finished solutions to all problems

--- Lecture1/lecture1.py
+++ Lecture1/lecture1.py
@@ -67,15 +67,30 @@ import math
 # print(f"After {years} years at {annual_interest}% interest ${initial_balance} "
 #       f"will turn into ${final_balance}")
 
-print("End of Testing")
 
 # Quadratic Formula to find zeros of polynomial ax^2 + bx + c
 # Create variables to store coefficients a, b, c
+# root = (-b - sqrt(b^2 - 4ac)) / 2a
+# a = 1
+# b = -4
+# c = 4
+# discriminant = b*b - 4*a*c
+# root1 = (-b + math.sqrt(discriminant)) / (2 * a)
+# root2 = (-b - math.sqrt(discriminant)) / (2 * a)
+# print(f"The roots of {a}x^2+{b}x+{c} are {root1} and {root2}")
+
 
 
 # Area of a triangle given the lengths of its 3 sides a, b, and c (Heron's Formula)
 # Create variables to store lengths of three sides side1, side2, side3
-# YOUR CODE HERE
+# side1 = 10
+# side2 = 10
+# side3 = 10
+# s = (side1 + side2 + side3) / 2
+# raw_area = math.sqrt(s * (s - side1) * (s - side2) * (s - side3))
+# area = round(raw_area,2)
+# print(f"The are of a rectangle with sides {side1}, {side2}, {side3} is {area}")
 
+print("End of Testing")
 
 print('End of Examples')
