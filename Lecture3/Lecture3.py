462d96b72a347c431e60d6be66b81764ceb2d1d5
BienvenidoVelez <bvelez@axiomatica.net>
2025-02-19 13:09:10 -0400

    Added Lecture 3 problems

--- Lecture3/Lecture3.py
+++ Lecture3/Lecture3.py
@@ -2,3 +2,42 @@
 # Lecture 3: Functions and Modules
 # Live Coding Examples
 
+# say_hello() function
+# def say_hello(name):
+#     print(f"Hello {name}")
+# print("GoodBye")
+#
+# say_hello("Bienve", "Velez")
+# say_hello("Juan")
+
+
+# say_hello(name) function
+
+
+# call say_hello
+
+
+# Fahrenheit to Celsius function
+# C = (F - 32) * (5/9)
+def fahrenheit_to_celsius(fahrenheit):
+    return (fahrenheit - 32) * (5 / 9)
+
+print(fahrenheit_to_celsius(60))
+
+
+
+# Celsius to Kelvin Function
+# K = C + 273.15
+def celsius_to_kelvin(celsius):
+    return celsius + 273.15s
+
+def fahrenheit_to_kelvin(fahrenheit):
+    return celsius_to_kelvin(fahrenheit_to_celsius(fahrenheit))
+
+print("End of tests")
+
+# say_hello(name) function with default parameter
+
+
+
+# Fahrenheit to Kelvin Function
