84cb744cfc7ed1ee94d0c5c0cc08c77791dfbb28
BienvenidoVelez <bvelez@axiomatica.net>
2025-02-19 14:47:42 -0400

    Added Lecture4_3.py

---
+++ Lecuture4/Lecture4_3.py
@@ -0,0 +1,23 @@
+import random
+
+# Cashier example that keeps track of the total per item and prints out the
+# total after you are done scanning everything.
+
+
+
+# Create a calculator that receives a number, an operand and a second number
+# and returns the result of the two numbers and the operator. Ask the user
+# after every iteration if they want to continue, and break out of the loop
+# if they are done.
+
+
+
+
+# Play the guessing a random number game again, but now implement a limited
+# number of tries.
+
+
+
+
+# Play the rock-paper-scissors game, but now implement an ongoing score and
+# first to five wins.
