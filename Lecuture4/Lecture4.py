0195497535bf53adcc310d74fd68a9beb3419345
BienvenidoVelez <bvelez@axiomatica.net>
2025-02-19 13:13:29 -0400

    Added Lecture4.py still empty.

---
+++ Lecuture4/Lecture4.py
@@ -0,0 +1,16 @@
+# UPRM CIIC 3015 Spring 2025
+# Lecture 4: Loops and Iteration
+# Live Coding Examples
+
+# Function to print numbers from 1 to n
+
+
+
+# Function read a password until it is correct
+
+
+
+#Function to decrement a timer until it reaches 0
+
+
+
