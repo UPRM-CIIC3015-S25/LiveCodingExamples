6e056b050947081542b10f696c88324dfc3ccb2b
BienvenidoVelez <bvelez@axiomatica.net>
2025-03-07 13:21:54 -0400

    Added solutions

--- Lecuture4/Lecture4_1.py
+++ Lecuture4/Lecture4_1.py
@@ -1,18 +1,56 @@
+import time
+
 # UPRM CIIC 3015 Spring 2025
 # Lecture 4: Loops and Iteration
 # Live Coding Examples
 
+# Function read a password until it is correct
+def get_password():
+    password = ''
+    while password != 'helloworld':
+        password = input('Password?')
+        if password == 'helloworld':
+            print('Access granted!')
+        else:
+            print('Wrong password.')
+    print("The End")
 
 
 # Function to print numbers from 1 to n
+def print_1_to_n(n):
+    next_number = 1
+    while next_number <= n:
+        print(f"Next number is {next_number}")
+        next_number += 1
+    print("End of while")
 
-
-
-# Function read a password until it is correct
-
-
+# print(print_1_to_n(4))
 
 #Function to decrement a timer every second until it reaches 0
+def countdown(secs):
+    while secs > 0:
+        print(f"{secs} seconds to blastoff")
+        time.sleep(1)
+        secs -= 1
+    print("Blastoff!!!")
+
+# countdown(10)
+
+# while True:
+#     print("Infinite")
+
+def print_rectangle(n,m):
+  print('\n')
+  for i in range(0, n):
+    for j in range (0,m):
+       print('*', end='')
+    print('')
+
+print_rectangle(10,20)
+
+
+print("End")
+
 
 
 
---
+++ Lecuture4/Lecture4_6.py
@@ -0,0 +1,19 @@
+import time
+
+# UPRM CIIC 3015 Spring 2025
+# Lecture 4: Loops and Iteration
+# Live Coding Examples
+
+
+# Function to print all numbers from 0 to N using a for loop
+
+
+# Function to print all multiples of k from 0 to N.
+
+
+
+# Boolean function to determine if a number is prime.
+
+
+
+# Function to print a triangle pattern of '*' (Nested Loops)
