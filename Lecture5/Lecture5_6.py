7e5c8da18a6cf8645333d54fade768004533676d
BienvenidoVelez <bvelez@axiomatica.net>
2025-03-19 11:16:47 -0400

    Added Lecture 5

---
+++ Lecture5/Lecture5_6.py
@@ -0,0 +1,17 @@
+# UPRM CIIC 3015 Spring 2025
+# Lecture 5: Strings and Lists
+
+# Live Coding Example 6
+# Common list processing patterns
+
+# Determine if element with some property exists
+
+# Find position of first element with some property
+
+# Count occurrences of elements with some property
+
+# Calculate accumulation of elements (e.g. sum, product)
+
+# Find max and min
+
+
