ef3c756828d5ed24b4e000b8b1dabf043245e597
BienvenidoVelez <bvelez@axiomatica.net>
2025-04-02 11:11:35 -0400

    Added Lecture7_1.py

---
+++ Lecture7/Lecture7_1.py
@@ -0,0 +1,67 @@
+# UPRM CIIC 3015 Spring 2025
+# Lecture 6: Dictionaries and Sets
+
+# Live Coding Example 7
+# Recursion
+
+# Generate list of integers from "first" to "last" recursively
+def my_range(first, last):
+    pass
+
+# Version 2: More efficient
+def my_range2(first, last):
+    pass
+
+
+print(f"Numbers from {0} to {10} are: {my_range2(0,10)}")
+
+
+# Calculate the sum of the numbers in a list recursively
+def sum_list(l):
+    pass
+
+print(f"Sum of numbers in [1,2,3,4,5] is: {sum_list([1,2,3,4,5])}")
+
+
+# Calculate a to the bth power recursively
+def my_pow(a,b):
+    pass
+
+print(f"{5} to the {3} power is: {my_pow(5,3)}")
+
+
+
+# Determine if a string is a palindrome recursively
+def is_palindrome(w):
+    pass
+
+print(f"Word \'{'radar'}\' is a palindrome? {is_palindrome('radar')}")
+print(f"Word \'{'bienve'}\' is a palindrome? {is_palindrome('bienve')}")
+print(f"Word \'{'b'}\' is a palindrome? {is_palindrome('b')}")
+print(f"Word \'{''}\' is a palindrome? {is_palindrome('')}")
+
+# Find the maximum among a list of numbers
+# Assume the list is not empty
+def max_recursive(l):
+    pass
+
+
+print(f"Maximum number in {[1,2,3,4,5]} is {max_recursive([1,2,3,4,5])}")
+print(f"Maximum number in {[5,4,3,2,1]} is {max_recursive([5,4,3,2,1])}")
+print(f"Maximum number in {[5]} is {max_recursive([5])}")
+
+
+# Reverse a string recursively
+def reverse_recursive(w):
+    pass
+
+print(f"Reverse of {"bienvenido"} is: {reverse_recursive("bienvenido")}")
+print(f"Reverse of {""} is: {reverse_recursive("")}")
+print(f"Reverse of {"b"} is: {reverse_recursive("b")}")
+
+
+#Return a list with all permutations of a string
+def permutations(w):
+    pass
+
+print(f"Permutations of {'eat'} are: {permutations('eat')}")
